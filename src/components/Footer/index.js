6244f04734cea72542c9bfafc467d2baed00b4be
Voitenko Ilya <79865476+IlyaVoitenko@users.noreply.github.com>
2023-07-15 19:55:19 +0300

    added Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-neutral-900 h-8 flex items-center justify-center ">
+      <span className="text-white">powered by DSEP</span>{" "}
+    </div>
+  );
+};
+
+export default Footer;
